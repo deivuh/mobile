d453dc03eb2f4324edb148fac1b4cda0da37b815
Taavi Rehemägi <taavi@toggl.com>
2015-01-09 10:34:08 +0200

    temp commit.

---
+++ Tests/Analytics/TestBaseTracker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using Toggl.Phoebe.Analytics;
+using XPlatUtils;
+using Toggl.Phoebe.Data;
+using Moq;
+
+namespace Toggl.Phoebe.Tests.Analytics
+{
+    [TestFixture]
+    public class TestBaseTracker : Test
+    {
+        private TestTracker tracker;
+
+        public override void SetUp ()
+        {
+            base.SetUp();
+            ServiceContainer.Register<ISettingsStore> (Mock.Of<ISettingsStore> (
+                        (store) => store.ExperimentId == (string)null));
+            ServiceContainer.Register<ExperimentManager> (new ExperimentManager ());
+            tracker = new TestTracker ();
+        }
+
+        [Test]
+        public void TestSendSettingsChangeEvent()
+        {
+            try {
+                tracker.SendSettingsChangeEvent(SettingName.AskForProject);
+            } catch (Exception e) {
+//                e.Data.
+                Console.WriteLine("e: {0}", e.Data.Contains("category"));
+            }
+        } 
+    }
+}
+
---
+++ Tests/Analytics/TestTracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Toggl.Phoebe.Analytics;
+
+namespace Toggl.Phoebe.Tests.Analytics
+{
+public class TestTracker : BaseTracker
+    {
+        protected override void StartNewSession()
+        {
+        }
+
+        protected override void SendTiming(long elapsedMilliseconds, string category, string variable, string label = null)
+        {
+        }
+
+        protected override void SendEvent(string category, string action, string label = null, long value = 0L)
+        {
+            throw new Exception(category);
+        }
+
+        protected override void SetCustomDimension(int idx, string value)
+        {
+        }
+
+        public override string CurrentScreen {
+            set {
+            }
+        }
+    }
+
+}
+
