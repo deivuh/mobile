278d7514ebc813a94542c9146eebbf950e989605
taavirehemagi <taavirehemagi@gmail.com>
2015-01-09 16:49:50 +0200

    temp commit.

--- Tests/Analytics/TestBaseTracker.cs
+++ Tests/Analytics/TestBaseTracker.cs
@@ -22,15 +22,97 @@ namespace Toggl.Phoebe.Tests.Analytics
         }
 
         [Test]
-        public void TestSendSettingsChangeEvent()
+        public void TestSendSettingsChangeEvent ()
         {
             try {
-                tracker.SendSettingsChangeEvent(SettingName.AskForProject);
+                tracker.SendSettingsChangeEvent (SettingName.AskForProject);
             } catch (Exception e) {
-//                e.Data.
-                Console.WriteLine("e: {0}", e.Data.Contains("category"));
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+                Assert.AreEqual (tracker.CurrentSendData.Label, "AskForProject");
             }
-        } 
+
+            try {
+                tracker.SendSettingsChangeEvent ((SettingName)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendAccountLoginEvent ()
+        {
+            try {
+                tracker.SendAccountLoginEvent (AccountCredentials.Password);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+                Assert.AreEqual (tracker.CurrentSendData.Label, "Password");
+            }
+
+            try {
+                tracker.SendAccountLoginEvent ((AccountCredentials)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendAccountCreateEvent ()
+        {
+            try {
+                tracker.SendAccountCreateEvent (AccountCredentials.Password);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+                Assert.AreEqual (tracker.CurrentSendData.Label, "Password");
+            }
+
+            try {
+                tracker.SendAccountCreateEvent ((AccountCredentials)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendAccountLogoutEvent ()
+        {
+            try {
+                tracker.SendAccountLogoutEvent ();
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendTimerStartEvent ()
+        {
+            try {
+                tracker.SendTimerStartEvent (TimerStartSource.AppNew);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+
+            try {
+                tracker.SendTimerStartEvent ((TimerStartSource)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
+
+        [Test]
+        public void TestSendTimerStopEvent ()
+        {
+            try {
+                tracker.SendTimerStopEvent (TimerStopSource.App);
+            } catch (Exception e) {
+                Assert.AreEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+
+            try {
+                tracker.SendTimerStopEvent ((TimerStopSource)100);
+            } catch (Exception e) {
+                Assert.AreNotEqual (TestTracker.SendEventExceptionMessage, e.Message);
+            }
+        }
     }
 }
 
